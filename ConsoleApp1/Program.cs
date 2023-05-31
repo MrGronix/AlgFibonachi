d56624187bd0acc232272a72285493eacf3e0020
Anton1442 <antonrrri142@gmail.com>
2023-05-31 09:17:04 +0300

    The prog was ending

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,16 @@
-﻿    static int Fibonacci(int a)
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        int a = 10;
+        Console.WriteLine($"Число Фібоначчі для числа {a} = {Fibonacci(a)}");
+    }
+
+    static int Fibonacci(int a)
     {
         if (a <= 0)
             return 0;
