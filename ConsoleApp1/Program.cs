f93f3d8c3854f4aa4149df8f22627543bdfa5790
Anton1442 <antonrrri142@gmail.com>
2023-05-31 09:34:45 +0300

    supp has a 2-nd half

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,22 +1,11 @@
-﻿using System;
-
-class Program
+﻿
+static int Fibonacci(int a)
 {
-    static void Main()
-    {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-        int a = 10;
-        Console.WriteLine($"Число Фібоначчі для числа {a} = {Fibonacci(a)}");
-    }
-
-    static int Fibonacci(int a)
-    {
-        if (a <= 0)
-            return 0;
-        else if (a == 1)
-            return 1;
-        else
-            return Fibonacci(a - 1) + Fibonacci(a - 2);
-    }
+    if (a <= 0)
+        return 0;
+    else if (a == 1)
+        return 1;
+    else
+        return Fibonacci(a - 1) + Fibonacci(a - 2);
+}
 }
